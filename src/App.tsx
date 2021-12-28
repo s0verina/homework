9f2b9bf31f29365c07875f48c59ae51a39ab3f5c
Julia <julia.poda@doxy.me>
2021-12-28 17:31:10 +0200

    Set up repo

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <main>
+       <h1>Homework</h1>
+    </main>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import './styles.scss';
+
+const app = document.getElementById("root");
+
+ReactDOM.render(<App />, app);
