0b0b4b272db2842be7822c17db3ff5e68df748f4
Julia Poda <s0verina@gmail.com>
2022-01-05 15:11:17 +0200

    Handle error for loading repo

--- src/actions/loadRepositories.ts
+++ src/actions/loadRepositories.ts
@@ -7,5 +7,5 @@ interface LoadReposProps {
 export async function loadRepositories<T> ({ owner }: LoadReposProps) {
   const url = `${GITHUB_API_URL}users/${owner}/repos`;
 
-  return fetcher<T>(url).catch(() => []);
+  return fetcher<T>(url);
 }
--- src/components/RepoAutocomplete/index.tsx
+++ src/components/RepoAutocomplete/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { useDebouncedCallback } from 'use-debounce';
-import { TextField, Autocomplete, CircularProgress } from '@mui/material';
+import { TextField, Autocomplete, CircularProgress, Box } from '@mui/material';
 import { loadRepositories } from '../../actions/loadRepositories';
 
 const DEBOUNCE_DELAY = 300;
 
 type RepoAutocompleteProps = {
   owner: string;
-  onChange: (value: string) => void;
+  onChange: (value: string, issuesCount: number) => void;
 }
 
 type Option = {
@@ -25,6 +25,7 @@ export const RepoAutocomplete: React.FC<RepoAutocompleteProps> = ({ owner, onCha
   const [selectedOption, setSelectedOptions] = React.useState<Option | null>(null);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
   const [options, setOptions] = React.useState<Option[]>([]);
+  const [error, setError] = React.useState<string>();
   const onSelect = React.useCallback((_, newValue: Option) => {
     setSelectedOptions(newValue);
     onChange(newValue.value, newValue.issuesCount);
@@ -32,48 +33,60 @@ export const RepoAutocomplete: React.FC<RepoAutocompleteProps> = ({ owner, onCha
   const debounceLoadOwnerRepos = useDebouncedCallback(async () => {
     if (owner) {
       setIsLoading(true);
-      const repos = await loadRepositories<Option[]>({ owner });
-      const newOptions = repos.map((repo: Repository) => ({
-        label: repo.name,
-        value: repo.name,
-        issuesCount: repo.open_issues_count
-      }));
-      setOptions(newOptions);
-      setIsLoading(false);
+      setError('');
+
+      try {
+        const repos = await loadRepositories<Option[]>({ owner });
+        const newOptions = repos.map((repo: Repository) => ({
+          label: repo.name,
+          value: repo.name,
+          issuesCount: repo.open_issues_count
+        }));
+        setOptions(newOptions);
+      } catch (err: unknown) {
+        if (err instanceof Error) {
+          setError(err?.message?.message as string);
+        }
+      } finally {
+        setIsLoading(false);
+      }
     }
   }, DEBOUNCE_DELAY);
 
   React.useEffect(() => {
     setSelectedOptions(null);
-    onChange('');
+    onChange('', 0);
     debounceLoadOwnerRepos();
   }, [owner]);
 
   return (
-    <Autocomplete
-        id="repo-name"
-        size="small"
-        options={options}
-        loading={isLoading}
-        disabled={!owner || isLoading}
-        value={selectedOption}
-        onChange={onSelect}
-        disableClearable
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Repository name"
-            InputProps={{
-              ...params.InputProps,
-              endAdornment: (
-                <React.Fragment>
-                  {isLoading ? <CircularProgress color="secondary" size={20} /> : null}
-                  {params.InputProps.endAdornment}
-                </React.Fragment>
-              ),
-            }}
-          />
-        )}
-      />
+    <>
+      <Autocomplete
+          id="repo-name"
+          size="small"
+          options={options}
+          loading={isLoading}
+          disabled={!owner || isLoading}
+          value={selectedOption}
+          onChange={onSelect}
+          disableClearable
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              label="Repository name"
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <React.Fragment>
+                    {isLoading ? <CircularProgress color="secondary" size={20} /> : null}
+                    {params.InputProps.endAdornment}
+                  </React.Fragment>
+                ),
+              }}
+            />
+          )}
+        />
+        {Boolean(error) && <Box sx={{ marginTop: 2, marginBottom: 1, color: 'error.main'}}>{error}</Box>}
+      </>
   );
 }
